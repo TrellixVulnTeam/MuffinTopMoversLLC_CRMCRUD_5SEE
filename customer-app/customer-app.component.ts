c254be3882b15f1987787f457ed7f4eb2f2f11d6
ByDesign21 <105322762+bydesign21@users.noreply.github.com>
2022-06-21 18:02:43 -0500

    Removed neccessary files

--- customer-app/customer-app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-@Component({
-  selector: 'customer-app-root',
-  templateUrl: './customer-app.component.html',
-  styleUrls: ['./customer-app.component.css']
-})
-export class CustomerAppComponent {
-  title = 'frontend';
-}
--- customer-app/read-customers/read.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-    $("[data-hide]").on("click", function(){
-        //$("." + $(this).attr("data-hide")).hide()
-        // -or-, see below
-         $(this).closest("." + $(this).attr("data-hide")).hide()
-    })
-})
